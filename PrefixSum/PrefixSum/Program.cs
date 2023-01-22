1c21862f98ec5481511a0cd637c9488194ba832e
frttnk <firattonak@gmail.com>
2023-01-21 23:55:19 -0500

    implement prefix sum

---
+++ PrefixSum/PrefixSum/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+
+
+int[] array = { 1, 2, 3, 4 };
+
+int[] prefixSum = new int[array.Length];
+prefixSum[0] = array[0];
+
+for (int i = 1; i < array.Length; i++)
+{
+    prefixSum[i] = prefixSum[i - 1] + array[i];
+}
+
+
+Console.WriteLine("Before Prefix");
+foreach (var item in array)
+{
+    Console.Write(item + " ");
+}
+
+Console.WriteLine("\nAfter Prefix");
+foreach (var item in prefixSum)
+{
+    Console.Write(item + " ");
+}
+
+
+
+
+
+
+
+
